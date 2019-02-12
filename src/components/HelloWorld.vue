466842fad00d913edc14af9de8875878d13957e5
deleman <computercodemohammad@gamil.com>
2019-02-12 03:41:45 -0800

    first complete diffrent betwin refrence type and primitive type in vuejs

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,19 +3,38 @@
     <h1>{{ msg }}</h1>
     <hr color="orange">
     <app-header></app-header>
+    <refrence-type v-bind:data="data" v-bind:lang="lang_data" v-on:masterFunction="MasterUpdate($event)"></refrence-type>
+      <hr color="red" width="90%">
+    <refrence-footer  v-bind:data="data" v-bind:lang="lang_data" v-on:masterFunction="MasterUpdate($event)"></refrence-footer>
   </div>
 </template>
 
 <script>
-import Header from './Header.vue';
+import Header from './Header';
+import Refrence from './Refrence';
+import Footer from './Footer';
+
 export default {
   components:{
     'app-header': Header,
+    'refrence-type':Refrence,
+    'refrence-footer':Footer,
   },
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      data:[
+        {name: 'mohammad' , email:'computercode@gamil.com',show:false},
+        {name: 'shnia', email: 'shnia@gmail.com', show: false},
+        {name: 'hedieh', email: 'hedieh@gmail.com', show: false}
+      ],
+      lang_data:'php progaraming and laravel framework',
+    }
+  },
+  methods:{
+    MasterUpdate(newText){
+      this.lang_data=newText;
     }
   }
 }
