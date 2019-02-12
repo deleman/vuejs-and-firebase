4eb966c883c3d21f77a20e85834013b05ba5af0b
deleman <computercodemohammad@gamil.com>
2019-02-12 01:45:50 -0800

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1 v-on:[ce]="show = !show" >file header </h1>
+    <p v-bind:[type]="value" v-show="show">{{info}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      info: 'my name is vue-cli',
+      type:'class',
+      value:'red',
+      ce:'click',
+      show:true
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style scoped>
+    p{
+      border:2px solid cyan;
+    }
+    .red{
+      color:red;
+    }
+</style>
